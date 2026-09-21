5e696aa3998847775ed27ce97bb4dba9f36a083c
Эльдар Каюмов <eldar13121997@mail.ru>
2026-09-21 19:10:12 +0300

    Добавлен ApplicationDbContext с поддержкой InspectionObject

---
+++ SoftwareAccountingService.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftwareAccountingService.Api.Domain.Entities;
+
+namespace SoftwareAccountingService.Api.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<InspectionObject> InspectionObjects { get; set; }
+    }
+}
