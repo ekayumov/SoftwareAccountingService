e8d39b45d86ea2ff6f9cb2518be29129d9066632
Эльдар Каюмов <eldar13121997@mail.ru>
2026-09-22 11:44:54 +0300

    реализован интерфейс IInspectionObjectService

---
+++ SoftwareAccountingService.Api/Services/IInspectionObjectService.cs
@@ -0,0 +1,22 @@
+﻿using SoftwareAccountingService.Api.Domain.Emuns;
+using SoftwareAccountingService.Api.DTO;
+
+namespace SoftwareAccountingService.Api.Services
+{
+    public interface IInspectionObjectService
+    {
+        Task<List<InspectionObjectDto>> GetAllAsync(
+            CancellationToken cancellationToken,
+            string? search,
+            InspectionResult? result,
+            InspectionType? type);
+
+        Task<InspectionObjectDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+        Task<InspectionObjectDto> CreateAsync(CreateInspectionObjectDto dto, CancellationToken cancellationToken);
+
+        Task<InspectionObjectDto> UpdateResultAsync(Guid id, UpdateInspectionResultDto dto, CancellationToken cancellationToken);
+
+        InspectionFilterOptionsDto GetFilterOptions();
+    }
+}
