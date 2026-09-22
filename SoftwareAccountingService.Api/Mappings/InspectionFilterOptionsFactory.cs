b15edfa3f06e36a99c0df3fa4a2acfa701498237
Эльдар Каюмов <eldar13121997@mail.ru>
2026-09-22 12:01:21 +0300

    GetFilterOptions() вынес логику в отдельную фабрику InspectionFilterOptionsFactory

---
+++ SoftwareAccountingService.Api/Mappings/InspectionFilterOptionsFactory.cs
@@ -0,0 +1,47 @@
+﻿using SoftwareAccountingService.Api.Domain.Enums;
+using SoftwareAccountingService.Api.DTO;
+
+namespace SoftwareAccountingService.Api.Mappings
+{
+    public static class InspectionFilterOptionsFactory
+    {
+        public static InspectionFilterOptionsDto Create()
+        {
+            return new InspectionFilterOptionsDto
+            {
+                Types = new List<FilterOptionDto>
+                {
+                    new FilterOptionDto
+                    {
+                        Code = InspectionType.SW.ToString(),
+                        DisplayName = "ПО (программное обеспечение)"
+                    },
+                    new FilterOptionDto
+                    {
+                        Code = InspectionType.HSC.ToString(),
+                        DisplayName = "ПАК (программно-аппаратный комплекс)"
+                    }
+                },
+
+                Results = new List<FilterOptionDto>
+                {
+                    new FilterOptionDto
+                    {
+                        Code = InspectionResult.InProgress.ToString(),
+                        DisplayName = "В работе"
+                    },
+                    new FilterOptionDto
+                    {
+                        Code = InspectionResult.Compliant.ToString(),
+                        DisplayName = "Соответствует"
+                    },
+                    new FilterOptionDto
+                    {
+                        Code = InspectionResult.NonCompliant.ToString(),
+                        DisplayName = "Не соответствует"
+                    }
+                }
+            };
+        }
+    }
+}
--- SoftwareAccountingService.Api/Services/InspectionObjectService.cs
+++ SoftwareAccountingService.Api/Services/InspectionObjectService.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SoftwareAccountingService.Api.Data;
 using SoftwareAccountingService.Api.Domain.Entities;
-using SoftwareAccountingService.Api.DTO;
 using SoftwareAccountingService.Api.Domain.Enums;
+using SoftwareAccountingService.Api.DTO;
+using SoftwareAccountingService.Api.Mappings;
 
 
 
@@ -128,41 +129,7 @@ namespace SoftwareAccountingService.Api.Services
 
         public InspectionFilterOptionsDto GetFilterOptions()
         {
-            return new InspectionFilterOptionsDto
-            {
-                Types = new List<FilterOptionDto>
-                {
-                    new FilterOptionDto
-                    {
-                        Code = InspectionType.SW.ToString(),
-                        DisplayName = "ПО (программное обеспечение)"
-                    },
-                    new FilterOptionDto
-                    {
-                        Code = InspectionType.HSC.ToString(),
-                        DisplayName = "ПАК (программно-аппаратный комплекс)"
-                    }
-                },
-
-                Results = new List<FilterOptionDto>
-                {
-                    new FilterOptionDto
-                    {
-                        Code = InspectionResult.InProgress.ToString(),
-                        DisplayName = "В работе"
-                    },
-                    new FilterOptionDto
-                    {
-                        Code = InspectionResult.Compliant.ToString(),
-                        DisplayName = "Соответствует"
-                    },
-                    new FilterOptionDto
-                    {
-                        Code = InspectionResult.NonCompliant.ToString(),
-                        DisplayName = "Не соответствует"
-                    }
-                }
-            };
+            return InspectionFilterOptionsFactory.Create();
         }
     }
 }
