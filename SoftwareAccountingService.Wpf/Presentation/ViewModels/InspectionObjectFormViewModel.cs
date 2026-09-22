6692de2e99c9919820807c0db015da0ac03948b7
Эльдар Каюмов <eldar13121997@mail.ru>
2026-09-22 16:21:38 +0300

    Добавлены ViewModel для объектов проверки и формы

---
+++ SoftwareAccountingService.Wpf/Presentation/ViewModels/InspectionObjectFormViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SoftwareAccountingService.Wpf.Presentation.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareAccountingService.Wpf.Presentation.ViewModels
+{
+    public class InspectionObjectFormViewModel : ObservableObject, INavigationAware
+    {
+        private readonly INavigationService _navigationService;
+
+        [ObservableProperty]
+        private Guid? inspectionObjectId;
+
+        [ObservableProperty]
+        private bool isEditMode;
+
+        [ObservableProperty]
+        private string pageTitle = "Новый объект";
+        public InspectionObjectFormViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+        public Task OnNavigatedToAsync(object? parameter)
+        {
+            if (parameter is Guid id)
+            {
+                InspectionObjectId = id;
+                IsEditMode = true;
+                PageTitle = "Редактирование объекта";
+            }
+            else
+            {
+                InspectionObjectId = null;
+                IsEditMode = false;
+                PageTitle = "Новый объект";
+            }
+
+            return Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private Task CancelAsync()
+        {
+            return _navigationService.NavigateToListAsync();
+        }
+    }
+ }
---
+++ SoftwareAccountingService.Wpf/Presentation/ViewModels/InspectionObjectsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SoftwareAccountingService.Wpf.Presentation.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareAccountingService.Wpf.Presentation.ViewModels
+{
+    public class InspectionObjectsViewModel : ObservableObject, INavigationAware
+    {
+        private readonly INavigationService _navigationService;
+        public InspectionObjectsViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public Task OnNavigatedToAsync(object? parameter)
+        {
+            // Позже здесь загрузим список объектов через API.
+            return Task.CompletedTask;
+        }
+
+        [RelayCommand]
+        private Task OpenCreateFormAsync()
+        {
+            return _navigationService.NavigateToFormAsync();
+        }
+
+        [RelayCommand]
+        private Task OpenEditFormAsync(
+            Guid inspectionObjectId)
+        {
+            return _navigationService.NavigateToFormAsync(
+                inspectionObjectId);
+        }
+    }
+}
